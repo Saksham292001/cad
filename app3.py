e01fec91acff6478c6b444a9552677b1104e0886
Saksham292001 <singhjaswalsaksham123456@gmail.com>
2022-05-29 20:21:32 +0530

    iuhiuh

--- app3.py
+++ app3.py
@@ -21,7 +21,7 @@ if active_tab not in tabs:
 li_items = "".join(
     f"""
     <li class="nav-item">
-        <a class="nav-link{' active' if t==active_tab else ''}" href="/?tab={t}">{t}</a>
+        <a class="nav-link{' active' if t==active_tab else ''}" href="https://share.streamlit.io/saksham292001/cad/app3.py?tab={t}">{t}</a>
     </li>
     """
     for t in tabs
