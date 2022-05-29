2d8e7ac6afdc9aa763ba09a84f4673a6915ec2c5
Saksham292001 <singhjaswalsaksham123456@gmail.com>
2022-05-29 20:44:47 +0530

    iuhiuh

--- app3.py
+++ app3.py
@@ -10,7 +10,8 @@ st.markdown(
 query_params = st.experimental_get_query_params()
 tabs = ["Movie Recommending System", "Project Detail", "Improvements"]
 if "tab" in query_params:
-    active_tab = query_params["tab"][0]
+    active_tab = query_params["tab"][]
+    print(query_params)
 else:
     active_tab = "Home"
 
